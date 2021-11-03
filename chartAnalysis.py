5ee0f8bf7a62ba0f1b1798da8a2b46a8a0ba8394
中嶌一樹 <kzknkjm1102@gmail.com>
2021-11-03 20:32:04 +0900

    second commit

--- chartAnalysis.py
+++ chartAnalysis.py
@@ -4,20 +4,26 @@ import numpy as np
 import pandas_datareader.data as web
 import pandas as pd
 
-# ax.set_yscale('log')
-
-def main():
-    symbol = 'TSLA'#AAPL,TSLA,MSFT,NVDA
-    start = dt.date(2020,1,1)
-    end = dt.date.today().strftime("%Y-%m-%d")
-
+def trans_xy(start,end):
     df = web.DataReader(symbol,"yahoo",start,end)
     df_xy = pd.DataFrame({symbol: df['Close']})#'Close' is the closing price
-    df_xy.plot()
+    return df_xy
+
+def plt_subset():
     plt.grid(color="gray",linestyle="--")
     plt.xticks(rotation=30)
     plt.legend().remove()
     plt.title('Historical Stock Price '+symbol)
+
+def main():
+    global symbol
+    symbol = 'TSLA'#AAPL,TSLA,MSFT,NVDA
+    start = dt.date(2018,1,1)
+    end = dt.date.today().strftime("%Y-%m-%d")
+
+    trans_xy(start,end).plot()
+    plt_subset()
+    # plt.yscale('log')#y軸を対数表示したい場合
     plt.show()
 
 if __name__ == '__main__':
